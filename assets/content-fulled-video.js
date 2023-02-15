f1f74bbab195f22d33011405b7838d95713c35fb
maxdanko-crocode <danko.crocode@gmail.com>
2023-02-15 19:06:17 +0300

    Work at video section

--- assets/content-fulled-video.js
+++ assets/content-fulled-video.js
@@ -3,11 +3,17 @@ class ContentFulledVideo {
         this.selectors = {
             tabLabel: ".js-content-fulled-video-label",
             tabInput: ".js-content-fulled-video-input",
-            activeLabel: ".content-fulled-video__tabs-label.content-fulled-video__tabs-label--active"
+            activeLabel: ".content-fulled-video__tabs-label.content-fulled-video__tabs-label--active",
+            buttonPlay: ".js-content-fulled-video-button-play",
+            videoContainer: ".js-content-fulled-video-container",
+            template: ".js-content-fulled-video-template",
+            videoContent: ".js-video-youtube, .js-video-vimeo",
+            bottomContainer: ".js-content-fulled-video-bottom-container"
         }   
         
         this.classes = {
-            activeTab: "content-fulled-video__tabs-label--active"
+            activeTab: "content-fulled-video__tabs-label--active",
+            hidden: "hidden"
         }
 
         this.container = container;
@@ -17,6 +23,8 @@ class ContentFulledVideo {
 
     init() {
         this.tabChange();
+        this.initVideo();
+        this.initBottomSectionVariants();
     }
 
     tabChange() {
@@ -44,6 +52,52 @@ class ContentFulledVideo {
 						.classList.add(this.classes.activeTab);
 			});
 		});
+    }
+
+    initVideo() {
+        const buttonPlay = this.container.querySelector(this.selectors.buttonPlay);
+
+		if (buttonPlay.classList.contains(this.classes.hidden)) {
+			buttonPlay.classList.remove(this.classes.hidden);
+		}
+
+		buttonPlay.addEventListener("click", () => {
+			const content = document.createElement("div");
+			const videoContainer = this.container.querySelector(
+				this.selectors.videoContainer
+			);
+			const template = videoContainer.querySelector(this.selectors.template);
+
+			content.appendChild(
+				template.content.firstElementChild.cloneNode(true)
+			);
+
+			if (content.querySelector(this.selectors.videoContent)) {
+				const video = content.querySelector(this.selectors.videoContent);
+				videoContainer.appendChild(video);
+
+				if (!buttonPlay.classList.contains(this.classes.hidden)) {
+					buttonPlay.classList.add(this.classes.hidden);
+				}
+			}
+		});
+    }
+
+    initBottomSectionVariants() {
+        document.addEventListener("variant:isbox", (event) => {
+            const bottomVariants = this.container.querySelector(this.selectors.bottomContainer);
+            const incommingId = +event.detail;
+            const currentId = +this.container.dataset.isBoxVariantId;
+            const isBox = incommingId === currentId;
+            
+            if (bottomVariants) {
+                if (isBox && isBox === true) {
+                    bottomVariants.classList.add(this.classes.hidden);
+                } else {
+                    bottomVariants.classList.remove(this.classes.hidden);
+                }
+            }
+        });
     }
 }
 
